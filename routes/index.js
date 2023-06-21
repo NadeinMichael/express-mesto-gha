a674a49f5cb432954d6b3df3bbc797fe75d08853
VladTolmachev <tolmachev368884@mail.ru>
2023-06-21 09:54:34 +0300

    fix: fix 404 error

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,8 @@ const cardRoutes = require('./cards');
 
 router.use('/users', userRoutes);
 router.use('/cards', cardRoutes);
-router.get('*', (req, res) => {
-  res.status(404).send({ message: '404' });
+router.use('*', (req, res) => {
+  res.status(404).send({ message: 'Page not found 404' });
 });
 
 module.exports = router;
